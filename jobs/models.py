b5ec60bbf2991fad4b4238e97053e72cde4b1411
Your Name <crystalline.rosette@gmail.com>
2021-04-02 15:14:18 -0400

    initial setup

--- jobs/models.py
+++ jobs/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.summary
